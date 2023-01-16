2d82a26bafb318cf17cbdb1021a5c3cbefa59783
dawnkelly09 <dawnkelly09@gmail.com>
2023-01-15 19:28:03 -0800

    adds textbook, update text, adds image

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,55 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState } from 'react'
+import Head from 'next/head'
+import Image from 'next/image'
+import nuggs from '../assets/Nuggs.png'
 
 const Home = () => {
+
+const [userInput, setUserInput] = useState('')
+const onUserChangedText = (event) => {
+  //use this console.log statement to test text input, then comment out
+  //console.log(event.target.value)
+  setUserInput(event.target.value)
+}
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Nugg - virtual budtender</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <Image src={nuggs} 
+                    alt='logo'
+                    width={240}
+                    height={308} />
+            <h1>Meet Nugg</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Your virtual budtender for navigating the legal cannabis marketplace.</h2>
+          </div>
+          <div className='prompt-instructions'>
+            <p>Tell Nugg what you are looking for in your cannabis experience and get a strain suggestion!</p>
+          </div>
+        </div>
+        <div className="prompt-container">
+          <textarea 
+            placeholder="Examples: 'help falling asleep', 'relieve back pain', 'boost creativity'" 
+            className="prompt-box"
+            value={userInput}
+            onChange={onUserChangedText} 
+            />
+          <div className='prompt-buttons'>
+            <a className='generate-button' onClick={null}>
+              <div className='generate'>
+                <p>Generate</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
-      <div className="badge-container grow">
+      {/*<div className="badge-container grow">
         <a
           href="https://buildspace.so/builds/ai-writer"
           target="_blank"
@@ -29,9 +60,9 @@ const Home = () => {
             <p>build with buildspace</p>
           </div>
         </a>
-      </div>
+  </div>*/}
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
